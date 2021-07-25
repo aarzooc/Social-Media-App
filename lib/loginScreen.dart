f08121b2007488c8c9f87a634c1f94b05c842090
JessicakJung <jungjess@student.ubc.ca>
2021-07-24 17:15:12 -0700

    login page UI

---
+++ lib/loginScreen.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget{
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>{
+
+  bool? isRememberMe = false;
+
+  Widget buildEmail(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Email',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+      SizedBox(height: 10),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 6,
+              offset: Offset(0,2)
+            )
+          ]
+        ),
+        height: 60, 
+        child: TextField(
+          keyboardType: TextInputType.emailAddress,
+          style: TextStyle(
+            color: Colors.black38
+          ),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.only(top:14),
+            prefixIcon: Icon(
+              Icons.email,
+              color: Color(0xffE2BC00)
+            ),
+            hintText: 'Email',
+            hintStyle: TextStyle(
+              color: Colors.black38
+            )
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+Widget buildPassword(){
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        'Password',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+      SizedBox(height: 10),
+      Container(
+        alignment: Alignment.centerLeft,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 6,
+              offset: Offset(0,2)
+            )
+          ]
+        ),
+        height: 60, 
+        child: TextField(
+          obscureText: true,
+          style: TextStyle(
+            color: Colors.black38
+          ),
+          decoration: InputDecoration(
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.only(top:14),
+            prefixIcon: Icon(
+              Icons.lock,
+              color: Color(0xffE2BC00)
+            ),
+            hintText: 'Password',
+            hintStyle: TextStyle(
+              color: Colors.black38
+            )
+          ),
+        ),
+      )
+    ],
+  );
+}
+
+Widget buildForgotPassBtn(){
+  return Container(
+    alignment: Alignment.centerRight,
+    child: FlatButton(
+      onPressed: () => print("Forgot Password pressed"),
+      padding: EdgeInsets.only(right:0),
+      child: Text(
+        'Forgot Password?',
+        style: TextStyle(
+          color: Colors.black87,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    ),
+  );
+}
+
+Widget buildRememberCb(){
+  return Container(
+    height: 20,
+    child: Row(
+      children: <Widget>[
+        Theme(
+          data: ThemeData(unselectedWidgetColor: Colors.black87),
+          child: Checkbox(
+            value: isRememberMe,
+            checkColor: Colors.black87,
+            activeColor: Colors.white,
+            onChanged: (value) {
+              setState(() {
+                isRememberMe = value;
+              });
+            },
+          ),
+        ),
+        Text(
+          'Remember Me',
+          style: TextStyle(
+            color: Colors.black87,
+            fontWeight: FontWeight.bold
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+Widget buildLoginBtn(){
+  return Container (
+    padding: EdgeInsets.symmetric(vertical: 25),
+    width: double.infinity,
+    child: RaisedButton(
+      elevation: 5,
+      onPressed: () => print('Login Pressed'),
+      padding: EdgeInsets.all(15),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15)
+      ),
+      color: Color(0xffE2BC00),
+      child: Text(
+        'SIGN IN',
+        style: TextStyle(
+          color: Colors.black87,
+          fontSize: 18,
+          fontWeight: FontWeight.bold
+        ),
+      ),
+    ),
+  );
+}
+
+Widget buildSignUpBtn() {
+  return GestureDetector(
+    onTap: () => print("Sign Up Pressed"),
+    child: RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(
+            text: 'Don\'t have an account? ',
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 18,
+              fontWeight: FontWeight.w500
+            )
+          ),
+          TextSpan(
+            text: 'Sign Up',
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 18,
+              fontWeight: FontWeight.bold
+            )
+          )
+        ]
+      ),
+    ),
+  );
+}
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                height: double.infinity,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Color(0xF200B8B5), //aqua
+                      Color(0xD900B8B5), //aqua
+                      Color(0x9C00B8B5), //aqua
+                      Color(0x8000B8B5), //aqua                     
+                    ]
+                  )
+                ),
+                child: SingleChildScrollView(
+                  physics: AlwaysScrollableScrollPhysics(),
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 25,
+                    vertical: 120
+                  ),
+                  child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Welcome to  HospiCHAT',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    SizedBox(height: 50,),
+                    buildEmail(),
+                    SizedBox(height: 20,),
+                    buildPassword(),
+                    buildForgotPassBtn(),
+                    buildRememberCb(),
+                    buildLoginBtn(),
+                    buildSignUpBtn(),
+                  ],
+                ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'loginScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,24 +10,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter Login UI',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen()
+      //theme: ThemeData(
+        //primarySwatch: Colors.blue,
+      // ),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+/*
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
@@ -111,3 +106,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
